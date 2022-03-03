0b454134a84e0df90b9abe4c36942398fa33ec50
Movas <42815276+Movas021@users.noreply.github.com>
2022-03-03 14:58:34 +0100

    Add files via upload

---
+++ authenticationAPI.py
@@ -0,0 +1,23 @@
+from flask import Flask,request
+import json
+app = Flask(__name__)
+
+@app.route('/api/v1/result', methods=['GET'])
+def hello_world():
+    with open('data.json', 'r') as database:
+        data = json.loads(database.read())
+    print(data)
+
+    key = request.args.get('api')
+
+    for user in data['users']:
+        if key == user['key']:
+            return "Access granted! your status is " + user['status']
+        else:
+            return "Sorry your status is not Active"
+
+
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
